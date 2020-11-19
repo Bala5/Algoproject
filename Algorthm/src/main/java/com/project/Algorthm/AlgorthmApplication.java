4f7dd33463c2be3e209b3d8c6c3ad1138f80d00c
user <user@DESKTOP-JOIK1LT>
2020-11-19 22:44:58 +0530

    Initial Setup

---
+++ Algorthm/src/main/java/com/project/Algorthm/AlgorthmApplication.java
@@ -0,0 +1,13 @@
+package com.project.Algorthm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorthmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorthmApplication.class, args);
+	}
+
+}
---
+++ Algorthm/src/test/java/com/project/Algorthm/AlgorthmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Algorthm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorthmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
